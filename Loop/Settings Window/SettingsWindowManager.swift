3f4859c877cdc3d2f5420fc4eb156fd0e335b0e6
Kai <hi@mrkai77.dev>
2025-12-06 14:40:07 -0700

    🐞 Improved settings window managment (#916)
    * ✨ New settings window management
    
    * 🚛 Tab -> SettingsTab

---
+++ Loop/Settings Window/SettingsWindowManager.swift
@@ -0,0 +1,131 @@
+//
+//  SettingsWindowManager.swift
+//  Loop
+//
+//  Created by Kai Azim on 2024-05-28.
+//
+
+import Combine
+import Defaults
+import Luminare
+import OSLog
+import SwiftUI
+
+final class SettingsWindowManager: ObservableObject {
+    static let shared = SettingsWindowManager()
+    private let logger = Logger(category: "SettingsWindowManager")
+    private var controller: NSWindowController?
+    private var previewActionTimerTask: Task<(), Error>?
+
+    @Published private(set) var previewedAction: WindowAction
+
+    @Published var showRadialMenu: Bool = false
+    @Published var showPreview: Bool = false
+
+    @Published var currentTab: SettingsTab = .icon {
+        didSet {
+            if currentTab == .radialMenu {
+                showRadialMenu = true
+                showPreview = false
+            } else if currentTab == .preview {
+                showRadialMenu = false
+                showPreview = true
+            } else {
+                showRadialMenu = true
+                showPreview = true
+            }
+        }
+    }
+
+    @Published var showInspector: Bool = true {
+        didSet {
+            if showInspector {
+                startTimer()
+            } else {
+                stopTimer()
+            }
+        }
+    }
+
+    let radialMenuViewModel: RadialMenuViewModel
+
+    var window: NSWindow? {
+        controller?.window
+    }
+
+    private init() {
+        let startingAction: WindowAction = .init(.topHalf)
+
+        self.previewedAction = startingAction
+        self.radialMenuViewModel = .init(startingAction: startingAction, window: nil, previewMode: true)
+    }
+
+    func show() {
+        if let controller, let window = controller.window {
+            window.orderFrontRegardless()
+        } else {
+            let window = LuminareWindow {
+                SettingsContentView(model: self)
+                    .frame(height: 620)
+            }
+
+            controller = NSWindowController(window: window)
+
+            SkyLightToolBelt.setBackgroundBlur(
+                windowID: CGWindowID(window.windowNumber),
+                radius: 20
+            )
+
+            window.backgroundColor = .white.withAlphaComponent(0.001)
+            window.ignoresMouseEvents = false
+            window.orderFrontRegardless()
+        }
+
+        if #available(macOS 14.0, *) {
+            NSApp.activate()
+        } else {
+            NSApp.activate(ignoringOtherApps: true)
+        }
+
+        startTimer()
+        NSApp.setActivationPolicy(.regular)
+
+        logger.log("Settings window opened")
+    }
+
+    func close() {
+        if let controller {
+            controller.close()
+            self.controller = nil
+        }
+
+        stopTimer()
+
+        if !Defaults[.showDockIcon] {
+            NSApp.setActivationPolicy(.accessory)
+        }
+
+        logger.log("Settings window closed")
+    }
+
+    private func startTimer() {
+        previewActionTimerTask?.cancel()
+        previewActionTimerTask = Task(priority: .utility) {
+            while true {
+                try await Task.sleep(for: .seconds(1))
+
+                if await controller?.window?.isKeyWindow == true, !Task.isCancelled {
+                    await MainActor.run {
+                        previewedAction.direction = previewedAction.direction.nextPreviewDirection
+                        radialMenuViewModel.setAction(to: previewedAction)
+                    }
+                }
+            }
+        }
+    }
+
+    private func stopTimer() {
+        previewActionTimerTask?.cancel()
+        previewActionTimerTask = nil
+    }
+}
